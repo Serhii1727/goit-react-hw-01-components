aca37c0de8afa1e54f5d8b9dcb7f52ac0252429d
Serhii Kravchenko <kravchenko087@ukr.net>
2022-06-24 23:15:33 +0300

    добавил компоненты в проект

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,20 @@
+import { FriendList } from './FriendList/FriendList';
+import { FriendListItem } from './FriendListItem/FriendListItem';
 import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import user from '../user.json';
+console.log(user);
 
 export const App = () => {
   return (
     <div>
       <Profile />
+      <Statistics />
+      <FriendList />
+      <FriendListItem />
+      <TransactionHistory />
+
     </div>
 
   );

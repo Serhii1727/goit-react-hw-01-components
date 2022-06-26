192cd6eb42059fc5d9a3a4433fb658b1396e1951
Serhii Kravchenko <kravchenko087@ukr.net>
2022-06-26 21:27:00 +0300

    добавил props в компонент Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,30 @@ import { FriendListItem } from './FriendListItem/FriendListItem';
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-import user from '../user.json';
+import user from '../user-info-json/user.json';
+import data from '../user-info-json/data.json';
+import friends from '../user-info-json/friends';
+import transactions from '../user-info-json/transactions.json'
+
 console.log(user);
+console.log(data);
+console.log(friends);
+console.log(transactions);
 
 export const App = () => {
   return (
     <div>
-      <Profile />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics />
       <FriendList />
       <FriendListItem />
       <TransactionHistory />
-
     </div>
 
   );
